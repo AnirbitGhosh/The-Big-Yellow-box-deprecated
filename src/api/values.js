8d49b7fb3e2763a772cda385d561ce445e3578eb
Anirbit Ghosh <2439281g@student.gla.ac.uk>
2021-08-07 23:50:46 +0530

    trial commit with require

--- src/api/values.js
+++ src/api/values.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://yellow-box-db.herokuapp.com/values",
+    baseURL: require("https://yellow-box-db.herokuapp.com/values"),
 });
